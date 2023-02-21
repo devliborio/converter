997677b99a54c5bb4b76231ea9121f7c765d6070
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-21 16:44:57 -0300

    Loading the table class and defining it in the main function

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var Reader = require("./Class/Reader"); // Importando leitor para o arquivo principal.
-var Processor = require("./Class/Processor"); // Importando Processador de dados.
+let Processor = require("./Class/Processor"); // Importando Processador de dados.
+let Table = require("./Class/Table"); // Importando Tabela.
 
 
-var reader = new Reader();
+let reader = new Reader();
 
 async function main() {
-    var data = await reader.Read('./users.csv');
-    var processedData = Processor.Process(data);
+    let data = await reader.Read('./users.csv');
+    let processedData = Processor.Process(data);
+    let users = new Table(processedData);
 }
 
 main();
