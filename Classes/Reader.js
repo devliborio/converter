c785bc02a954bcc09c22f273c58dbd8d0edd6682
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-21 16:12:34 -0300

    Converting reader to work asynchronously using async / await

--- Classes/Reader.js
+++ Classes/Reader.js
@@ -1,17 +1,20 @@
 const fs = require('fs'); // Importando File System
+const util = require('util');
 
 class Reader {
 
-    Read(filepath) {
-        fs.readFile(filepath, "utf-8", (err, data) => {
-
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(data);
-            }
-        });
+    constructor() {
+        this.reader = util.promisify(fs.readFile); // Convertendo o fs.readFile para uma função que funciona com o async / await
     }
-}
+
+    async Read(filepath) {
+        try {
+            return await this.reader(filepath, 'utf-8')
+
+        } catch (erro) {
+            return undefined;
+        }
+    }
+};
 
 module.exports = Reader;
