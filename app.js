ed15c84f9c05b3c6a8d5b1357e6740f19657cd64
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-21 16:31:28 -0300

    Receiving Data Processor functionalities

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var Reader = require("./Classes/Reader"); // Importando leitor para o arquivo principal.
+var Processor = require("./Classes/Processor"); // Importando Processador de dados.
+
 
 var reader = new Reader();
 
-async function main(){
+async function main() {
     var data = await reader.Read('./users.csv');
-    console.log(data);
+    var processedData = Processor.Process(data);
 }
 
 main();
