566769019a83ff9c42af0ae33345fcdd916ba673
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-22 15:39:48 -0300

    Importing writer and setting variable in global scope for use in main function

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 let Reader = require("./Class/Reader"); // Importando leitor para o arquivo principal.
+let Writer = require("./Class/Writer"); // Importando o escritor 
 let Processor = require("./Class/Processor"); // Importando Processador de dados.
 let Table = require("./Class/Table"); // Importando Tabela.
 let HtmlParser = require("./Class/HtmlParser"); // Importando Conversor de ".csv" para HTMl (string).
 
 
+
 let reader = new Reader();
+let writer = new Writer();
 
 async function main() {
     let data = await reader.Read('./users.csv');
     let processedData = Processor.Process(data);
     let users = new Table(processedData);
     let html = await HtmlParser.Parse(users);
+
+    writer.Write("htmlConvertido.html", html); // Arquivo gerado pelo escritor, convertendo assim o users.csv para um arquivo HTML com a formatação tabulada.
  
 }
 
