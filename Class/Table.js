867c1b8bf9f48466ba1d73a3fec26ef0e9771a19
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-22 15:18:16 -0300

    adjusting tabs

--- Class/Table.js
+++ Class/Table.js
@@ -1,16 +1,16 @@
-class Table{
+class Table {
 
-    constructor(arr){
+    constructor(arr) {
         this.header = arr[0]; // Pegando o cabeçalho.
         arr.shift(); // Removendo o cabeçalho, para que sobre somente as linhas.
         this.rows = arr; // Pegando as linhas.
-    } 
+    }
 
-    get RowCount(){ // Campo Virtual
+    get RowCount() { // Campo Virtual
         return this.rows.length; // Retornando quantidade de linhas da tabela.
     }
 
-    get ColumCount(){ // Campo Virtual
+    get ColumCount() { // Campo Virtual
         return this.header.length; // Retornando quantidade de colunas da tabela.
     }
 
