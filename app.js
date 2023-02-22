26c927b34e876adb32c37b6c56e051355748c7d0
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-22 16:09:16 -0300

    Importing the PDF writer and calling its method to perform the html to pdf conversion

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let Writer = require("./Class/Writer"); // Importando o escritor
 let Processor = require("./Class/Processor"); // Importando Processador de dados.
 let Table = require("./Class/Table"); // Importando Tabela.
 let HtmlParser = require("./Class/HtmlParser"); // Importando Conversor de ".csv" para HTMl (string).
+let PDFwriter = require("./Class/PDFwriter"); // Importando escritor de PDF.
 
 
 
@@ -16,7 +17,9 @@ async function main() {
     let html = await HtmlParser.Parse(users);
 
     writer.Write(Date.now() + ".html", html); // Arquivo gerado pelo escritor, convertendo assim o users.csv para um arquivo HTML com a formatação tabulada.
- 
+    PDFwriter.writePDF(Date.now() + ".pdf", html); // Arquivo gerado pelo escritor de PDF, convertendo assim o users.csv para um arquivo PDF com a mesma estrutura do arquivo HTML.
+
+    
 }
 
 main();
