25375e905bdeaab5a6805806d4f2b927de03d450
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-22 15:11:44 -0300

    sending value of rows variable to EJS

--- Class/HtmlParser.js
+++ Class/HtmlParser.js
@@ -3,7 +3,7 @@ const ejs = require('ejs'); // Carregando EJS
 class HtmlParser {
 
     static async Parse(table) {
-        return await ejs.renderFile("./views/table.ejs", { header: table.header }); // Ele vai pegar o arquivo, ver o que tem dentro dele, e processar o arquivo!
+        return await ejs.renderFile("./views/table.ejs", { header: table.header, rows: table.rows }); // Ele vai pegar o arquivo, ver o que tem dentro dele, e processar o arquivo!
     }
 }
 
