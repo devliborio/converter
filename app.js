aa8d2cc3f87b2e9ea014d242fbec58df9760608d
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-22 14:59:19 -0300

    Importing HtmlParser class

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var Reader = require("./Class/Reader"); // Importando leitor para o arquivo principal.
+let Reader = require("./Class/Reader"); // Importando leitor para o arquivo principal.
 let Processor = require("./Class/Processor"); // Importando Processador de dados.
 let Table = require("./Class/Table"); // Importando Tabela.
+let HtmlParser = require("./Class/HtmlParser"); // Importando Conversor de ".csv" para HTMl (string).
 
 
 let reader = new Reader();
